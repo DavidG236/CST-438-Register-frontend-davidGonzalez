96bd39fbb73c99b0a039ab79c1ae0c6ea1cb7567
David <davidgonzalez1@csumb.edu>
2022-10-04 18:17:26 -0700

    implement feature add student

--- src/components/Semester.js
+++ src/components/Semester.js
@@ -62,7 +62,15 @@ class Semester extends Component {
                 variant="outlined" color="primary" style={{margin: 10}}>
                 Get Schedule
               </Button>
+              <Button component={Link} 
+                      to={{pathname:'/addStudent' , 
+                      year:SEMESTER_LIST[this.state.selected].year, 
+                      semester:SEMESTER_LIST[this.state.selected].name}} 
+                variant="outlined" color="primary" style={{margin: 10}}>
+                Add Student
+              </Button>
           </div>
+
       </div>
     )
   }
